2a71b58c5eb36b1d14e0b9a2dd7a715f01c7f13f
tonya <antonina.cherednichenko@gmail.com>
2016-02-26 16:33:40 +0200

    Fix signature format method

--- src/utils.ts
+++ src/utils.ts
@@ -18,7 +18,7 @@ export function formFnSignatureForPath(sig: string): string {
     if (sig.indexOf('{') !== -1) {
         sig = sig.substring(0, sig.indexOf('{'));
     }
-    return sig.trim().replace(';', '').replace(' ', '').replace(/:\s*/g, "_");;
+    return sig.trim().replace(/;\s*/g, "").replace(/:\s*/g, "_").replace(/ \s*/g, "");
 }
 
 export var PATH_SEPARATOR: string = ".";
