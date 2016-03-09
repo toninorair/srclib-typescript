cf61784144606f7e2f10d01a8fd6112b2e80d1e5
tonya <antonina.cherednichenko@gmail.com>
2016-03-09 14:42:22 +0200

    Add file for automatic tsconfig.sh generation, can be used for projects where this file is absent

---
+++ generate-tsconfig.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+# This script generates tsconfig.json file, it looks for all files with extension .ts and adds them to the file section. 
+# Old tsconfig.json file, if exists is saved with .ol.timestamp extension
+
+FILE_NAME="tsconfig.json"
+
+if [ -f $FILE_NAME ];
+then
+  timestamp=$(date +%s)
+  mv $FILE_NAME "$FILE_NAME.old.$timestamp"
+fi
+
+echo "{
+  \"compilerOptions\": {
+    \"module\": \"commonjs\",
+    \"target\": \"es5\",
+    \"outDir\": \".bin\"
+  },
+  \"files\": [" >> $FILE_NAME
+
+for file in $(find . -name *.ts)
+do
+  echo "\"$file\"", >> $FILE_NAME
+done
+
+sed -i.bk "$ s/.$//" $FILE_NAME 
+
+echo "]}" >> $FILE_NAME
+ 
+rm -rf $FILE_NAME.bk
